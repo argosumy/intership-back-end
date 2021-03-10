55c2f0d334773635de04116ab4efbd849bcdcb52
IlyaSpilnichenko <spilnich@gmail.com>
2021-03-10 17:44:55 +0200

    hotfix/acm-004 Use @RestControllerAdvice.

--- src/main/java/com/spd/baraholka/comment_reactions/exceptions/CommentReactionExceptionAdvice.java
+++ src/main/java/com/spd/baraholka/comment_reactions/exceptions/CommentReactionExceptionAdvice.java
@@ -2,19 +2,18 @@ package com.spd.baraholka.comment_reactions.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class CommentReactionExceptionAdvice {
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ExceptionHandler(CommentReactionNoContentException.class)
     String commentReactionNoContentHandler(CommentReactionNoContentException e) {
         return e.getMessage();
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(CommentReactionBadRequestException.class)
     String commentReactionNoContentHandler(CommentReactionBadRequestException e) {
