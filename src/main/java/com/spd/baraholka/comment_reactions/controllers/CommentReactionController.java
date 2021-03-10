aa426f9dda649780fb58910f4e390db4cf0333bc
IlyaSpilnichenko <spilnich@gmail.com>
2021-03-10 17:34:51 +0200

    hotfix/acm-004 Delete null check for @RequestBody in CommentReaction.

--- src/main/java/com/spd/baraholka/comment_reactions/controllers/CommentReactionController.java
+++ src/main/java/com/spd/baraholka/comment_reactions/controllers/CommentReactionController.java
@@ -3,8 +3,6 @@ package com.spd.baraholka.comment_reactions.controllers;
 import com.spd.baraholka.comment_reactions.dto.CommentReactionDto;
 import com.spd.baraholka.comment_reactions.entities.CommentReaction;
 import com.spd.baraholka.comment_reactions.enums.CommentReactionType;
-import com.spd.baraholka.comment_reactions.exceptions.CommentReactionBadRequestException;
-import com.spd.baraholka.comment_reactions.exceptions.CommentReactionNoContentException;
 import com.spd.baraholka.comment_reactions.services.CommentReactionService;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,17 +18,11 @@ public class CommentReactionController {
     @GetMapping("/comment-reactions/{commentId}/{commentReaction}")
     public int getTotalReactionTypeByCommentId(@PathVariable("commentId") int commentId,
                                                @PathVariable("commentReaction") CommentReactionType commentReactionType) {
-        if (commentReactionType == null) {
-            throw new CommentReactionBadRequestException();
-        }
         return commentReactionService.getTotalReactionTypeByCommentId(commentId, commentReactionType);
     }
 
     @PostMapping("/comment-reaction")
     public CommentReaction saveNew(@RequestBody CommentReactionDto commentReactionDto) {
-        if (commentReactionDto == null) {
-            throw new CommentReactionNoContentException();
-        }
         CommentReaction commentReaction = CommentReactionDto.toCommentReaction(commentReactionDto);
         return commentReactionService.saveNew(commentReaction);
     }
@@ -38,9 +30,6 @@ public class CommentReactionController {
     @DeleteMapping("/comment-reaction/{commentId}/{commentReaction}")
     public void deleteCommentReaction(@PathVariable("commentId") int commentId,
                                       @PathVariable("commentReaction") CommentReactionType commentReactionType) {
-        if (commentReactionType == null) {
-            throw new CommentReactionBadRequestException();
-        }
         commentReactionService.deleteLastRecordByCommentId(commentId, commentReactionType);
     }
 }
