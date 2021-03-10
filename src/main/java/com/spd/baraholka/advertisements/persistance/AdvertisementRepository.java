0b65cc002e47d797c2a4c38cab6979e4011b5a4a
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-10 15:40:28 +0200

    BE-AMC-002 Fix key generating in AdvertisementRepository and number of parameters in prepared statement.

--- src/main/java/com/spd/baraholka/advertisements/persistance/AdvertisementRepository.java
+++ src/main/java/com/spd/baraholka/advertisements/persistance/AdvertisementRepository.java
@@ -2,10 +2,10 @@ package com.spd.baraholka.advertisements.persistance;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Objects;
 
@@ -22,7 +22,7 @@ public class AdvertisementRepository {
             + "status,"
             + "creation_date,"
             + "publication_date,"
-            + "status_change_date) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+            + "status_change_date) VALUES (?,?,?,?,?,?,?,?,?,?,?) RETURNING id";
     private final JdbcTemplate jdbcTemplate;
 
     public AdvertisementRepository(JdbcTemplate jdbcTemplate) {
@@ -30,20 +30,20 @@ public class AdvertisementRepository {
     }
 
     public int addAdvertisement(Advertisement advertisement) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(con -> {
-            final PreparedStatement statement = con.prepareStatement(INSERT_ADVERTISEMENT);
+            PreparedStatement statement = con.prepareStatement(INSERT_ADVERTISEMENT, Statement.RETURN_GENERATED_KEYS);
             statement.setInt(1, advertisement.getOwnerId());
             statement.setString(2, advertisement.getTitle());
-            statement.setString(2, advertisement.getDescription());
-            statement.setString(3, advertisement.getCategory());
-            statement.setDouble(4, advertisement.getPrice());
-            statement.setString(5, advertisement.getCurrency().toString());
-            statement.setString(6, advertisement.getCity());
-            statement.setString(7, advertisement.getStatus().toString());
-            statement.setTimestamp(8, Timestamp.valueOf(advertisement.getCreationDate()));
-            statement.setTimestamp(9, Timestamp.valueOf(advertisement.getPublicationDate()));
-            statement.setTimestamp(10, Timestamp.valueOf(advertisement.getStatusChangeDate()));
+            statement.setString(3, advertisement.getDescription());
+            statement.setString(4, advertisement.getCategory());
+            statement.setDouble(5, advertisement.getPrice());
+            statement.setString(6, advertisement.getCurrency().toString());
+            statement.setString(7, advertisement.getCity());
+            statement.setString(8, advertisement.getStatus().toString());
+            statement.setTimestamp(9, Timestamp.valueOf(advertisement.getCreationDate()));
+            statement.setTimestamp(10, Timestamp.valueOf(advertisement.getPublicationDate()));
+            statement.setTimestamp(11, Timestamp.valueOf(advertisement.getStatusChangeDate()));
             return statement;
         }, keyHolder);
         return Objects.requireNonNull(keyHolder.getKey()).intValue();
