7f47d9f9f451a3ffb0b2baf8a04c119456d9e575
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-10 13:33:29 +0200

    BE-AMC-002 Add implementation for AdvertisementDTO class.

--- src/main/java/com/spd/baraholka/advertisements/services/AdvertisementDTO.java
+++ src/main/java/com/spd/baraholka/advertisements/services/AdvertisementDTO.java
@@ -1,4 +1,118 @@
 package com.spd.baraholka.advertisements.services;
 
+import com.spd.baraholka.advertisements.persistance.AdvertisementStatus;
+import com.spd.baraholka.advertisements.persistance.CurrencyType;
+
+import java.time.LocalDateTime;
+
 public class AdvertisementDTO {
+
+    private int ownerId;
+    private String title;
+    private String description;
+    private String category;
+    private double price;
+    private CurrencyType currency;
+    private boolean discountAvailability;
+    private String city;
+    private AdvertisementStatus status;
+    private LocalDateTime creationDate;
+    private LocalDateTime publicationDate;
+    private LocalDateTime statusChangeDate;
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public CurrencyType getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(CurrencyType currency) {
+        this.currency = currency;
+    }
+
+    public boolean isDiscountAvailability() {
+        return discountAvailability;
+    }
+
+    public void setDiscountAvailability(boolean discountAvailability) {
+        this.discountAvailability = discountAvailability;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public AdvertisementStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AdvertisementStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDateTime publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public LocalDateTime getStatusChangeDate() {
+        return statusChangeDate;
+    }
+
+    public void setStatusChangeDate(LocalDateTime statusChangeDate) {
+        this.statusChangeDate = statusChangeDate;
+    }
 }
