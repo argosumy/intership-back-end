7fe84bfc173edf8213c40de530f252f6e5ef4bf8
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-10 19:53:09 +0200

    BE-AMC-001 Remove not null constrains from 'advertisement' table.

--- src/main/resources/db/migration/V20210310095333__create_advertisements_table.sql
+++ src/main/resources/db/migration/V20210310095333__create_advertisements_table.sql
@@ -1,16 +1,16 @@
 CREATE TABLE advertisements
 (
-    id                    SERIAL PRIMARY KEY         NOT NULL,
-    user_id               int                        NOT NULL,
-    title                 VARCHAR(200)               NOT NULL,
-    description           TEXT                       NOT NULL,
-    category              VARCHAR                    NOT NULL,
-    price                 numeric CHECK ( price > 0) NOT NULL,
-    currency              VARCHAR(3)                 NOT NULL,
+    id                    SERIAL PRIMARY KEY NOT NULL,
+    user_id               int                NOT NULL,
+    title                 VARCHAR(200),
+    description           TEXT,
+    category              VARCHAR,
+    price                 numeric CHECK ( price > 0),
+    currency              VARCHAR(3),
     discount_availability BOOLEAN DEFAULT false,
-    city                  VARCHAR(20)                NOT NULL,
-    status                VARCHAR(15)                NOT NULL,
-    creation_date         TIMESTAMP                  NOT NULL,
-    publication_date      TIMESTAMP                  NOT NULL,
-    status_change_date    TIMESTAMP                  NOT NULL
+    city                  VARCHAR(20),
+    status                VARCHAR(15)        NOT NULL,
+    creation_date         TIMESTAMP          NOT NULL,
+    publication_date      TIMESTAMP,
+    status_change_date    TIMESTAMP          NOT NULL
 );
