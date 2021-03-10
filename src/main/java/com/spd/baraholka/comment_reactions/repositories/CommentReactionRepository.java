c7d0a16d4ea7a2b653823cb5fab07955faa39f1e
IlyaSpilnichenko <spilnich@gmail.com>
2021-03-10 19:14:20 +0200

    hotfix/acm-004 Use NamedParameterJdbcTemplate in CommentReactionRepository.

--- src/main/java/com/spd/baraholka/comment_reactions/repositories/CommentReactionRepository.java
+++ src/main/java/com/spd/baraholka/comment_reactions/repositories/CommentReactionRepository.java
@@ -3,25 +3,27 @@ package com.spd.baraholka.comment_reactions.repositories;
 
 import com.spd.baraholka.comment_reactions.entities.CommentReaction;
 import com.spd.baraholka.comment_reactions.enums.CommentReactionType;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class CommentReactionRepository implements CommentReactionPersistence {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public CommentReactionRepository(JdbcTemplate jdbcTemplate) {
+    public CommentReactionRepository(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public CommentReaction saveNew(CommentReaction commentReaction) {
         jdbcTemplate.update(
-                "INSERT INTO comment_reactions (reaction, comment_id, user_id) VALUES (?, ?, ?)",
-                commentReaction.getCommentReactionType().name(),
-                commentReaction.getComment().getId(),
-                commentReaction.getUser().getId()
+                "INSERT INTO comment_reactions (reaction, comment_id, user_id) VALUES (:reaction, :commentId, :userId)",
+                Map.of("reaction", commentReaction.getCommentReactionType().name(),
+                        "commentId", commentReaction.getComment().getId(),
+                        "userID", commentReaction.getUser().getId())
         );
         return commentReaction;
     }
@@ -30,8 +32,9 @@ public class CommentReactionRepository implements CommentReactionPersistence {
     public void deleteLastRecordByCommentId(int commentId, CommentReactionType commentReactionType) {
         jdbcTemplate.update(
                 "DELETE FROM comment_reactions WHERE id=" +
-                        "(SELECT id FROM comment_reactions WHERE comment_id=? AND reaction=? ORDER BY id DESC LIMIT 1)",
-                commentId, commentReactionType.name()
+                        "(SELECT id FROM comment_reactions WHERE comment_id=:commentId AND reaction=:reaction ORDER BY id DESC LIMIT 1)",
+                Map.of("commentId", commentId,
+                        "reaction", commentReactionType.name())
         );
     }
 
@@ -39,9 +42,10 @@ public class CommentReactionRepository implements CommentReactionPersistence {
     @SuppressWarnings("ConstantConditions")
     public int getTotalReactionTypeByCommentId(int commentId, CommentReactionType commentReactionType) {
         return jdbcTemplate.queryForObject(
-                "SELECT COUNT(id) FROM comment_reactions WHERE comment_id=? AND reaction=?",
-                Integer.class,
-                commentId, commentReactionType.name()
+                "SELECT COUNT(id) FROM comment_reactions WHERE comment_id=:commentId AND reaction=:reaction",
+                Map.of("commentId", commentId,
+                        "reaction", commentReactionType.name()),
+                Integer.class
         );
     }
 }
