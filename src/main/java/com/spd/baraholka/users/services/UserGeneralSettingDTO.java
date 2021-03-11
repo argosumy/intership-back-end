e01f50143befeb50a7dd9dd5bddefc014bcd337a
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-11 14:13:27 +0200

    BE-P-002 Add fields with get/set methods for UserGeneralSettingDTO class.

--- src/main/java/com/spd/baraholka/users/services/UserGeneralSettingDTO.java
+++ src/main/java/com/spd/baraholka/users/services/UserGeneralSettingDTO.java
@@ -1,4 +1,32 @@
 package com.spd.baraholka.users.services;
 
 public class UserGeneralSettingDTO {
+
+    private int id;
+    private int userId;
+    private boolean openAdsInNewTab;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isOpenAdsInNewTab() {
+        return openAdsInNewTab;
+    }
+
+    public void setOpenAdsInNewTab(boolean openAdsInNewTab) {
+        this.openAdsInNewTab = openAdsInNewTab;
+    }
 }
