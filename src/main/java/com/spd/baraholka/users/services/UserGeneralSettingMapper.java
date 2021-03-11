811983436ac71fb6a5da522dba305ca6649a05ef
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-11 14:16:26 +0200

    BE-P-002 Add UserGeneralSettingMapper :: convertToEntity().

--- src/main/java/com/spd/baraholka/users/services/UserGeneralSettingMapper.java
+++ src/main/java/com/spd/baraholka/users/services/UserGeneralSettingMapper.java
@@ -1,7 +1,16 @@
 package com.spd.baraholka.users.services;
 
+import com.spd.baraholka.users.persistance.UserGeneralSetting;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserGeneralSettingMapper {
+
+    UserGeneralSetting convertToEntity(UserGeneralSettingDTO userGeneralSettingDTO) {
+        UserGeneralSetting userGeneralSetting = new UserGeneralSetting();
+        userGeneralSetting.setId(userGeneralSettingDTO.getId());
+        userGeneralSetting.setUserId(userGeneralSetting.getUserId());
+        userGeneralSetting.setOpenAdsInNewTab(userGeneralSettingDTO.isOpenAdsInNewTab());
+        return userGeneralSetting;
+    }
 }
