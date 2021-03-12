7b376a4bf1a7d69cae55b5be9180ff912d050c49
IlyaSpilnichenko <spilnich@gmail.com>
2021-03-12 22:21:52 +0200

    hotfix/mp-002 Add advertisement row mapper.

--- src/main/java/com/spd/baraholka/advertisements/services/AdvertisementRowMapper.java
+++ src/main/java/com/spd/baraholka/advertisements/services/AdvertisementRowMapper.java
@@ -1,4 +1,32 @@
 package com.spd.baraholka.advertisements.services;
 
-public class AdvertisementRowMapper {
+import com.spd.baraholka.advertisements.persistance.Advertisement;
+import com.spd.baraholka.advertisements.persistance.AdvertisementStatus;
+import com.spd.baraholka.advertisements.persistance.CurrencyType;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class AdvertisementRowMapper implements RowMapper<Advertisement> {
+
+    @Override
+    public Advertisement mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Advertisement advertisement = new Advertisement();
+        advertisement.setAdvertisementId(rs.getInt("id"));
+        advertisement.setOwnerId(rs.getInt("ownerId"));
+        advertisement.setTitle(rs.getString("title"));
+        advertisement.setDescription(rs.getString("description"));
+        advertisement.setCategory(rs.getString("category"));
+        advertisement.setPrice(rs.getDouble("price"));
+        advertisement.setCurrency(CurrencyType.valueOf(rs.getString("currency")));
+        advertisement.setDiscountAvailability(rs.getBoolean("is_discount_available"));
+        advertisement.setCity(rs.getString("city"));
+        advertisement.setStatus(AdvertisementStatus.valueOf(rs.getString("status")));
+        advertisement.setCreationDate(rs.getDate("created_at"));
+        advertisement.setPublicationDate(LocalDate.of(rs.getString("publication_datetime")));
+        advertisement.setStatusChangeDate(LocalDate.of(rs.getDate("status_changed_at")));
+        return advertisement;
+    }
 }
