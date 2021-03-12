d1e3a6ac89c6199be6cb69bd313e0c71c1fe6f88
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-12 11:51:30 +0200

    BE-P-001 Add UserMapper :: convertToDTO method.

--- src/main/java/com/spd/baraholka/users/services/UserMapper.java
+++ src/main/java/com/spd/baraholka/users/services/UserMapper.java
@@ -10,12 +10,25 @@ public class UserMapper {
         User user = new User();
         user.setId(userDTO.getId());
         user.setFirstName(userDTO.getFirstName());
-        user.setLastName(user.getLastName());
+        user.setLastName(userDTO.getLastName());
         user.setEmail(userDTO.getEmail());
         user.setPosition(userDTO.getPosition());
         user.setPhoneNumber(userDTO.getPhoneNumber());
         user.setBlocked(userDTO.isBlocked());
         user.setAdditionalContactResources(userDTO.getAdditionalContactResources());
         return user;
+    }
+
+    public UserDTO convertToDTO(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPosition(user.getPosition());
+        userDTO.setPhoneNumber(user.getPhoneNumber());
+        userDTO.setBlocked(user.isBlocked());
+        userDTO.setAdditionalContactResources(user.getAdditionalContactResources());
+        return userDTO;
     }
 }
