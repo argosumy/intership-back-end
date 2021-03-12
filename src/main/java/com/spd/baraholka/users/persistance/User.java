c27f3bf4d2a733c3928aec6aa84b2697c561b117
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-12 10:24:08 +0200

    BE-P-001 Add implementation for User class.

--- src/main/java/com/spd/baraholka/users/persistance/User.java
+++ src/main/java/com/spd/baraholka/users/persistance/User.java
@@ -1,4 +1,79 @@
 package com.spd.baraholka.users.persistance;
 
+import java.util.Map;
+
 public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String position;
+    private String phoneNumber;
+    private boolean isBlocked;
+    private Map<String, String> additionalContactResources;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
+    }
+
+    public Map<String, String> getAdditionalContactResources() {
+        return additionalContactResources;
+    }
+
+    public void setAdditionalContactResources(Map<String, String> additionalContactResources) {
+        this.additionalContactResources = additionalContactResources;
+    }
 }
