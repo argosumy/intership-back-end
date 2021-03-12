c17990253a4c0779041ff7dd5cd7e7af4428287c
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-12 10:27:05 +0200

    BE-P-001 Add implementation for UserMapper class.

--- src/main/java/com/spd/baraholka/users/services/UserMapper.java
+++ src/main/java/com/spd/baraholka/users/services/UserMapper.java
@@ -1,7 +1,21 @@
 package com.spd.baraholka.users.services;
 
+import com.spd.baraholka.users.persistance.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
+
+    public User convertToEntity(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(user.getLastName());
+        user.setEmail(userDTO.getEmail());
+        user.setPosition(userDTO.getPosition());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+        user.setBlocked(userDTO.isBlocked());
+        user.setAdditionalContactResources(userDTO.getAdditionalContactResources());
+        return user;
+    }
 }
