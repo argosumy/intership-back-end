b6ecd651b653c44379e54c4d06b97dd5006dfcae
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-12 12:52:12 +0200

    BE-P-003 Led to a uniform style UserRepository methods.

--- src/main/java/com/spd/baraholka/users/persistance/UserRepository.java
+++ src/main/java/com/spd/baraholka/users/persistance/UserRepository.java
@@ -17,14 +17,22 @@ public class UserRepository {
     }
 
     public User selectUserById(int id) {
-        String selectSQL = "SELECT * FROM users WHERE id=:id";
-        return jdbcTemplate.queryForObject(selectSQL, Map.of("id", id), userRowMapper);
+        String selectSQL = createSelectUserByIDSQL();
+        return jdbcTemplate.queryForObject(selectSQL, fillSelectUserByIdParameters(id), userRowMapper);
     }
 
     public int updateUserMainInfo(User user) {
         String updateSQL = createUpdateUserMainInfoSQL();
         jdbcTemplate.update(updateSQL, fillUpdateUserMainInfoParameters(user));
         return user.getId();
+    }
+
+    private Map<String, Integer> fillSelectUserByIdParameters(int id) {
+        return Map.of("id", id);
+    }
+
+    private String createSelectUserByIDSQL() {
+        return "SELECT * FROM users WHERE id=:id";
     }
 
     private String createUpdateUserMainInfoSQL() {
