d19f6c1fa182ca74d096dd9182a1c4bcf5b3c653
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-16 09:52:00 +0200

    BE-p-001 Remove unused UserMapper :: convertToEntity() method.

--- src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
+++ src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
@@ -7,20 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
 
-    public User convertToEntity(UserDTO userDTO) {
-        User user = new User();
-        user.setId(userDTO.getId());
-        user.setFirstName(userDTO.getFirstName());
-        user.setLastName(userDTO.getLastName());
-        user.setEmail(userDTO.getEmail());
-        user.setPosition(userDTO.getPosition());
-        user.setPhoneNumber(userDTO.getPhoneNumber());
-        user.setBlocked(userDTO.isBlocked());
-        user.setAdditionalContactResources(userDTO.getAdditionalContactResources());
-        return user;
-    }
-
-    public UserDTO convertToDTO(User user){
+    public UserDTO convertToDTO(User user) {
         UserDTO userDTO = new UserDTO();
         userDTO.setId(user.getId());
         userDTO.setFirstName(user.getFirstName());
