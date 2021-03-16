158668fa49e2c25ee4bb8e4ca5c0a8582218ccbb
Valeriia Lapytska <lerkasan@gmail.com>
2021-03-16 12:28:19 -0400

    [BE-004] removed a volume storing database data, hardcoded database name, user, password, added script to populate a database with test data

---
+++ test-data/V333333333333333_populate_db.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, first_name, last_name, email, location, position, phone_number, avatar, is_blocked)
+VALUES (1, 'Christian', 'Caldwell', 'nibh.sit.amet@ac.com', 'Olinda', 'Donec Vitae Foundation', '38-033-832-36-96', 'https://s3.amazonaws.com/mksnvl', 'true');
+INSERT INTO users (id, first_name, last_name, email, location, position, phone_number, avatar, is_blocked)
+VALUES (2,'Kenneth','Rodriguez','libero@lectusCumsociis.co.uk','Kerkrade','Metus Company','38-056-046-15-06','https://s3.amazonaws.com/mksnvl','true');
