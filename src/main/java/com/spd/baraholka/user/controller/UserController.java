4ede98f57f17e4e2891ed8eceb449588d2f16e2f
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-17 20:05:08 +0200

    BE-p-004 Fix UserController request mapping.

--- src/main/java/com/spd/baraholka/user/controller/UserController.java
+++ src/main/java/com/spd/baraholka/user/controller/UserController.java
@@ -2,12 +2,10 @@ package com.spd.baraholka.user.controller;
 
 import com.spd.baraholka.user.controller.dto.UserDTO;
 import com.spd.baraholka.user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
