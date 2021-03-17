8fadb839cac3029804a3bbd0fecc7b721ba07051
Serhii Zahorui <zahoruiserhii@gmail.com>
2021-03-17 16:06:17 +0200

    Fixed migration for the 'advertisements_images' table. Add is_primary column.

--- src/main/resources/db/migration/V20210317155603__create_advirtisements_images_table.sql
+++ src/main/resources/db/migration/V20210317155603__create_advirtisements_images_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE advertisements_images
 (
-    id       SERIAL PRIMARY KEY,
-    ad_id    INT      NOT NULL,
-    image_id INT      NOT NULL REFERENCES images(id),
-    position SMALLINT NOT NULL
+    id         SERIAL PRIMARY KEY,
+    ad_id      INT      NOT NULL,
+    image_id   INT      NOT NULL REFERENCES images (id),
+    is_primary BOOLEAN  NOT NULL,
+    position   SMALLINT NOT NULL
 );
