f0c67745f22d5a28a8cd31fa08e753fdc875dc89
serya <serya42572@gmail.com>
2021-03-17 21:58:08 +0200

    resolve checkstyle-test errors

--- src/main/java/com/spd/baraholka/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.spd.baraholka.exception;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
