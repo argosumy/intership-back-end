707f194562fa70c5d53a46ab2e4efb2dba91b0e5
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-19 08:21:03 +0200

    BE-p-014 Add to UserMapper :: convertToDTOList()/convertToShortViewDTO methods.

--- src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
+++ src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
@@ -1,8 +1,13 @@
 package com.spd.baraholka.user.controller.mappers;
 
 import com.spd.baraholka.user.controller.dto.UserDTO;
+import com.spd.baraholka.user.controller.dto.UserShortViewDTO;
 import com.spd.baraholka.user.persistance.entities.User;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class UserMapper {
@@ -18,4 +23,19 @@ public class UserMapper {
         userDTO.setBlocked(user.isBlocked());
         return userDTO;
     }
+
+    public List<UserShortViewDTO> convertToDTOList(List<User> users) {
+        return users.stream().map(this::convertToShortViewDTO).collect(Collectors.toList());
+    }
+
+    private UserShortViewDTO convertToShortViewDTO(User user) {
+        UserShortViewDTO userDTO = new UserShortViewDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setBlocked(user.isBlocked());
+        userDTO.setEndDateOfBan(LocalDateTime.now()); //TODO add endDataOfBan field to User
+        return userDTO;
+    }
+
 }
