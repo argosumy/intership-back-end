15bf22deab76f39955329cd49d3ca30813e7614e
Sytnianskyi Oleksandr <sitnyanskij.aleksandr@gmail.com>
2021-03-19 10:48:31 +0000

    BE-P-001 Add needed dependencies into 'build.gradle' file.

---
+++ src/main/java/com/spd/baraholka/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.spd.baraholka.user.controller;
+
+import com.spd.baraholka.user.controller.dto.UserDTO;
+import com.spd.baraholka.user.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+}
