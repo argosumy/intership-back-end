3eadc67723a5baf4d8c4ed8a534d29778bd13f86
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-19 09:34:51 +0200

    BE-p-014 Implement selectAllUsers() method in UserRepository class.

--- src/main/java/com/spd/baraholka/user/persistance/repositories/UserRepository.java
+++ src/main/java/com/spd/baraholka/user/persistance/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.spd.baraholka.user.persistance.repositories;
 import com.spd.baraholka.user.persistance.PersistenceUserService;
 import com.spd.baraholka.user.persistance.mappers.UserRowMapper;
 import com.spd.baraholka.user.persistance.entities.User;
+import com.spd.baraholka.user.persistance.mappers.UserShortViewRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -13,10 +15,14 @@ public class UserRepository implements PersistenceUserService {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
     private final UserRowMapper userRowMapper;
+    private final UserShortViewRowMapper userShortViewRowMapper;
 
-    public UserRepository(NamedParameterJdbcTemplate jdbcTemplate, UserRowMapper userRowMapper) {
+    public UserRepository(NamedParameterJdbcTemplate jdbcTemplate,
+                          UserRowMapper userRowMapper,
+                          UserShortViewRowMapper userShortViewRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
         this.userRowMapper = userRowMapper;
+        this.userShortViewRowMapper = userShortViewRowMapper;
     }
 
     @Override
@@ -24,5 +30,11 @@ public class UserRepository implements PersistenceUserService {
         String selectSQL = "SELECT * FROM users WHERE id=:id";
         Map<String, Integer> selectParameters = Map.of("id", id);
         return jdbcTemplate.queryForObject(selectSQL, selectParameters, userRowMapper);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        String selectSQL = "SELECT id, first_name, last_name, is_blocked, end_date_of_ban FROM users";
+        return jdbcTemplate.query(selectSQL, userShortViewRowMapper);
     }
 }
