83bd27aac5ccf5b64f6c833e0df5d49a46f6f572
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-19 08:14:25 +0200

    BE-p-014 Add controller to return all users in short view.

--- src/main/java/com/spd/baraholka/user/controller/UserController.java
+++ src/main/java/com/spd/baraholka/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.spd.baraholka.user.controller;
 
 import com.spd.baraholka.user.controller.dto.UserDTO;
+import com.spd.baraholka.user.controller.dto.UserShortViewDTO;
 import com.spd.baraholka.user.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/users")
 @RestController
@@ -20,5 +23,10 @@ public class UserController {
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable int id) {
         return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<UserShortViewDTO> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
