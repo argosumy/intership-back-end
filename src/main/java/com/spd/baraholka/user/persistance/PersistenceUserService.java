340f8cb2d27d3a5e9ecdc07d397ef54f76422c03
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-19 09:34:08 +0200

    BE-p-014 Add PersistenceUserService :: selectAllUsers() method.

--- src/main/java/com/spd/baraholka/user/persistance/PersistenceUserService.java
+++ src/main/java/com/spd/baraholka/user/persistance/PersistenceUserService.java
@@ -2,7 +2,11 @@ package com.spd.baraholka.user.persistance;
 
 import com.spd.baraholka.user.persistance.entities.User;
 
+import java.util.List;
+
 public interface PersistenceUserService {
 
     User selectUserById(int id);
+
+    List<User> selectAllUsers();
 }
