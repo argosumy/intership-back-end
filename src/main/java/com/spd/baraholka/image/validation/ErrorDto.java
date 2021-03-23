bfc75611c245e1506d7779a209d706306e23e9d6
serhii_zahorui <zahoruiserhii@gmail.com>
2021-03-23 19:47:51 +0200

    Fixed checkstyle

--- src/main/java/com/spd/baraholka/image/validation/ErrorDto.java
+++ src/main/java/com/spd/baraholka/image/validation/ErrorDto.java
@@ -36,8 +36,12 @@ public class ErrorDto {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         ErrorDto errorDto = (ErrorDto) o;
         return status == errorDto.status &&
                 timeStamp == errorDto.timeStamp &&
