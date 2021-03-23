124087f7ab1011d257281fa0e6d9d0ba2fa9f076
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-23 14:41:09 +0200

    BE-p-001 Update test data inserting.

--- test-data/V20220317160826__test_data.sql
+++ test-data/V20220317160826__test_data.sql
@@ -1,11 +1,15 @@
-INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number)
-VALUES ('Ivan', 'Ivanov', 'Smila', 'ivan@spd-ukraine.com', 'developer', '+380685489524');
-INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number)
-VALUES ('Petua', 'Petrov', 'Kyiv', 'petua@spd-ukraine.com', 'developer', '+380676548921');
-INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number)
-VALUES ('Gena', 'Genov', 'Charkiv', 'gena@spd-ukraine.com', 'developer', '+380685686243');
-INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number)
-VALUES ('Vasua', 'Ivanov', 'Odessa', 'vasua@spd-ukraine.com', 'developer', '+38068658958');
+INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number, image_url)
+VALUES ('Ivan', 'Ivanov', 'Smila', 'ivan@spd-ukraine.com', 'developer', '+380685489524',
+        'https://baraholka-images-store.s3-eu-west-1.amazonaws.com/users/1/homer3.jpg');
+INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number, image_url)
+VALUES ('Petua', 'Petrov', 'Kyiv', 'petua@spd-ukraine.com', 'developer', '+380676548921',
+        'https://baraholka-images-store.s3-eu-west-1.amazonaws.com/users/1/homer3.jpg');
+INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number, image_url)
+VALUES ('Gena', 'Genov', 'Charkiv', 'gena@spd-ukraine.com', 'developer', '+380685686243',
+        'https://baraholka-images-store.s3-eu-west-1.amazonaws.com/users/1/homer3.jpg');
+INSERT INTO users (first_name, last_name, location, e_mail, position, phone_number, image_url)
+VALUES ('Vasua', 'Ivanov', 'Odessa', 'vasua@spd-ukraine.com', 'developer', '+38068658958',
+        'https://baraholka-images-store.s3-eu-west-1.amazonaws.com/users/1/homer3.jpg');
 INSERT INTO users_additional_resources (user_id, resource_name, resource_url)
 VALUES (1, 'vk', 'vk.com');
 INSERT INTO users_additional_resources (user_id, resource_name, resource_url)
