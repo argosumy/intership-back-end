97dffc44e3426a42abcb369251fd0a50b20cfef7
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-25 01:24:27 +0200

    BE-p-003 After merge fix.

--- src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
+++ src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
@@ -39,7 +39,7 @@ public class UserMapper {
         user.setFirstName(oAuth2UserDto.getFirstName());
         user.setLastName(oAuth2UserDto.getLastName());
         user.setEmail(oAuth2UserDto.getEmail());
-        user.setImageUrl(oAuth2UserDto.getAvatar());
+        user.setImageUrl(oAuth2UserDto.getImageUrl());
         user.setLocation("");
         user.setPosition("");
         user.setPhoneNumber("");
