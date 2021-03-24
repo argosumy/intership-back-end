2cf88bc8f65625afa0b80920690e75486a828042
Valeriia Lapytska <lerkasan@gmail.com>
2021-03-24 11:47:48 -0400

    [BE-BUG-002] added 403 Forbidden response for unauthorized requests instead of redirect to login form

--- src/main/java/com/spd/baraholka/config/SecurityConfig.java
+++ src/main/java/com/spd/baraholka/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -38,7 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
-                .successHandler(oauth2SuccessHandler);
+                .successHandler(oauth2SuccessHandler)
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint());
     }
 
     @Override
