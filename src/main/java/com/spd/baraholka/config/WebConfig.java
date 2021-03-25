c66b5e3c0e0052d799df8828d579c9dad9c6c8f8
Valeriia Lapytska <lerkasan@gmail.com>
2021-03-24 20:59:47 -0400

    [BE-CORS] added misc CORS configuration

--- src/main/java/com/spd/baraholka/config/WebConfig.java
+++ src/main/java/com/spd/baraholka/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-                .allowedOrigins("*");
+                .allowedOrigins("http://localhost:3002");
     }
 }
