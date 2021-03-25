862f0ccb7cf531d8a75becc0fc22e9fb80c91107
serya <serya42572@gmail.com>
2021-03-25 14:32:41 +0200

    changed CategoryDTOMapper

--- src/main/java/com/spd/baraholka/characteristic/persistance/mappers/CategoryDTOMapper.java
+++ src/main/java/com/spd/baraholka/characteristic/persistance/mappers/CategoryDTOMapper.java
@@ -4,15 +4,22 @@ import com.spd.baraholka.characteristic.persistance.dto.CategoryDTO;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class CategoryDTOMapper implements RowMapper<CategoryDTO> {
     @Override
     public CategoryDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new CategoryDTO(rs.getString("category_name"),
-                (Set<String>) rs.getArray("ar"));
+        Array sourceArray = rs.getArray("ar");
+        String[] array = (String[]) sourceArray.getArray();
+        Set<String> targetSet = new HashSet<>();
+        Collections.addAll(targetSet, array);
+
+        return new CategoryDTO(rs.getString("category_name"), targetSet);
     }
 }
