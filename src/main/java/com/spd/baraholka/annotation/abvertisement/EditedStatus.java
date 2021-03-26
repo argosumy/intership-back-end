188c7962e881ab8053de50e2a77b0f1a1cec050d
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-26 14:19:41 +0200

    BE-bug-006/BE-ACM-019 Create AdvertisementEditedStatusValidator class. Create EditedStatus annotation.

---
+++ src/main/java/com/spd/baraholka/annotation/abvertisement/EditedStatus.java
@@ -0,0 +1,22 @@
+package com.spd.baraholka.annotation.abvertisement;
+
+import com.spd.baraholka.validation.advertisement.AdvertisementEditedStatusValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = AdvertisementEditedStatusValidator.class)
+public @interface EditedStatus {
+
+    String message() default "Baraholka advertisement status annotation validation";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
