7e54ffe3fd665cca374390c8ef1098ecf3d7c11f
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-29 08:50:36 +0300

    BE-p-002 Add new 'endpoint' for change user general settings.

--- src/main/java/com/spd/baraholka/user/controller/UserController.java
+++ src/main/java/com/spd/baraholka/user/controller/UserController.java
@@ -2,23 +2,28 @@ package com.spd.baraholka.user.controller;
 
 import com.spd.baraholka.user.controller.dto.UserDTO;
 import com.spd.baraholka.user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spd.baraholka.user.service.UserSettingsService;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/users")
 @RestController
 public class UserController {
 
     private final UserService userService;
+    private final UserSettingsService userSettingsService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserSettingsService userSettingsService) {
         this.userService = userService;
+        this.userSettingsService = userSettingsService;
     }
 
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable int id) {
         return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}/general")
+    public int updateUserGeneralSettings(@PathVariable int id, @RequestParam("openAdsInNewTab") boolean openAdsInNewTab) {
+        return userSettingsService.updateUserGeneralSettings(id, openAdsInNewTab);
     }
 }
