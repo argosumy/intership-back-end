00e512f6eae15dc9f8f6626b2d066b5ece015789
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-29 21:53:00 +0300

    BE-P-019 Add AdvertisementController :: getAdvertisement() method and inject UserService class.

--- src/main/java/com/spd/baraholka/advertisement/controller/AdvertisementController.java
+++ src/main/java/com/spd/baraholka/advertisement/controller/AdvertisementController.java
@@ -1,10 +1,14 @@
 package com.spd.baraholka.advertisement.controller;
 
 import com.spd.baraholka.advertisement.controller.dto.EditedAdvertisementDTO;
+import com.spd.baraholka.advertisement.controller.dto.FullAdvertisementDTO;
 import com.spd.baraholka.advertisement.controller.dto.InitialAdvertisementDTO;
 import com.spd.baraholka.advertisement.persistance.entities.AdvertisementStatus;
 import com.spd.baraholka.advertisement.service.AdvertisementService;
+import com.spd.baraholka.annotation.advertisement.AdvertisementExist;
 import com.spd.baraholka.annotation.advertisement.ChangedStatus;
+import com.spd.baraholka.user.controller.dto.OwnerDTO;
+import com.spd.baraholka.user.service.UserService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +20,11 @@ import javax.validation.Valid;
 public class AdvertisementController {
 
     private final AdvertisementService advertisementService;
+    private final UserService userService;
 
-    public AdvertisementController(AdvertisementService advertisementService) {
+    public AdvertisementController(AdvertisementService advertisementService, UserService userService) {
         this.advertisementService = advertisementService;
+        this.userService = userService;
     }
 
     @PostMapping
@@ -34,5 +40,13 @@ public class AdvertisementController {
     @PutMapping("/{id}")
     public int updateAdvertisementStatus(@PathVariable int id, @RequestParam("status") @ChangedStatus AdvertisementStatus status) {
         return advertisementService.updateAdvertisementStatus(id, status);
+    }
+
+    @GetMapping("/{id}")
+    public FullAdvertisementDTO getAdvertisement(@PathVariable("id") @AdvertisementExist int id) {
+        FullAdvertisementDTO advertisementDTO = advertisementService.getAdvertisementById(id);
+        OwnerDTO ownerDTO = userService.getOwner(id);
+        advertisementDTO.setAdvertisementOwner(ownerDTO);
+        return advertisementDTO;
     }
 }
