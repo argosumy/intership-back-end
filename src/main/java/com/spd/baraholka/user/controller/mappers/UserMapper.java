75706c18c1aa5516e6eaf64f39a9b428c9a8f4ac
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-29 11:32:41 +0300

    BE-p-002 Add endDateOfBan and location to UserDTO with related logic for it.

--- src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
+++ src/main/java/com/spd/baraholka/user/controller/mappers/UserMapper.java
@@ -26,6 +26,8 @@ public class UserMapper {
         userDTO.setPhoneNumber(user.getPhoneNumber());
         userDTO.setBlocked(user.isBlocked());
         userDTO.setImageUrl(collapseImageUrl(user));
+        userDTO.setEndDateOfBan(user.getEndDateOfBan());
+        userDTO.setLocation(user.getLocation());
         return userDTO;
     }
 
