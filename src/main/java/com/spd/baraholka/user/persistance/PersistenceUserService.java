0972f8e8becbb0ac7e00f35c64a8f3b34ea6383a
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-29 23:45:14 +0300

    BE-P-019 Add PersistenceUserService :: selectOwner() method.

--- src/main/java/com/spd/baraholka/user/persistance/PersistenceUserService.java
+++ src/main/java/com/spd/baraholka/user/persistance/PersistenceUserService.java
@@ -1,5 +1,6 @@
 package com.spd.baraholka.user.persistance;
 
+import com.spd.baraholka.user.persistance.entities.Owner;
 import com.spd.baraholka.user.persistance.entities.User;
 
 import java.util.Optional;
@@ -18,4 +19,6 @@ public interface PersistenceUserService {
     List<User> selectAllUsers();
 
     Optional<Boolean> isExist(int value);
+
+    Owner selectOwner(int id);
 }
