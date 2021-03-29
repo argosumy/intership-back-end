4bb51c6b14e4506d872b3925d6fcd0b0a23ffbd3
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-03-29 08:49:53 +0300

    BE-p-002 Rename service, repository classes into 'UserSettings'. Replace dto logic to parameter in these classes.

--- src/main/java/com/spd/baraholka/user/persistance/PersistenceUserGeneralSettingService.java
+++
@@ -1,8 +0,0 @@
-package com.spd.baraholka.user.persistance;
-
-import com.spd.baraholka.user.persistance.entities.UserGeneralSetting;
-
-public interface PersistenceUserGeneralSettingService {
-
-    int updateUserGeneralSettings(UserGeneralSetting userGeneralSetting);
-}
