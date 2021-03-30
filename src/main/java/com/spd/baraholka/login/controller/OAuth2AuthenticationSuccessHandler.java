ea852a71fef9bd82284536deb78b88294b64778b
Valeriia Lapytska <lerkasan@gmail.com>
2021-03-30 13:54:34 -0400

    [BE-P-015] added redirect to root successful login

--- src/main/java/com/spd/baraholka/login/controller/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/spd/baraholka/login/controller/OAuth2AuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +13,12 @@ import java.io.IOException;
 
 @Component
 @Qualifier("OAuth2SuccessHandler")
-public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        response.sendRedirect("http://localhost:3002");
+        clearAuthenticationAttributes(request);
+        response.sendRedirect("/");
+//  For local development only
+//        response.sendRedirect("http://localhost:3002");
     }
 }
