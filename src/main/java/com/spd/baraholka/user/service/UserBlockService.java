d6fce3d0c120ab0ecbda0dd61ddbbf8277ccc065
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-01 14:07:35 +0300

    BE-P-004 Inject PersistenceUserBlockService in UserBlockService class.

--- src/main/java/com/spd/baraholka/user/service/UserBlockService.java
+++ src/main/java/com/spd/baraholka/user/service/UserBlockService.java
@@ -1,12 +1,19 @@
 package com.spd.baraholka.user.service;
 
 import com.spd.baraholka.user.controller.dto.BanDetailDTO;
+import com.spd.baraholka.user.persistance.PersistenceUserBlockService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserBlockService {
 
+    private final PersistenceUserBlockService persistenceUserBlockService;
+
+    public UserBlockService(PersistenceUserBlockService persistenceUserBlockService) {
+        this.persistenceUserBlockService = persistenceUserBlockService;
+    }
+
     public int changeUserBlockedStatus(BanDetailDTO banDetailDTO) {
-        return 0;
+        return persistenceUserBlockService.blockUser(banDetailDTO);
     }
 }
