4f548e296231c4509628ec0ebeb108b561ef249c
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-01 13:46:36 +0300

    BE-P-004 Add get/set methods for BanDetailDTO and 'min' userId value constraint.

--- src/main/java/com/spd/baraholka/user/controller/dto/BanDetailDTO.java
+++ src/main/java/com/spd/baraholka/user/controller/dto/BanDetailDTO.java
@@ -3,6 +3,7 @@ package com.spd.baraholka.user.controller.dto;
 import com.spd.baraholka.annotation.advertisement.PresentOrFutureDate;
 import com.spd.baraholka.annotation.user.UserExist;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 public class BanDetailDTO {
 
     @NotNull
+    @Min(1)
     @UserExist
     private int userId;
 
@@ -23,4 +25,36 @@ public class BanDetailDTO {
 
     @NotNull
     private boolean isNotify;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getBanedUntil() {
+        return banedUntil;
+    }
+
+    public void setBanedUntil(LocalDateTime banedUntil) {
+        this.banedUntil = banedUntil;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public boolean isNotify() {
+        return isNotify;
+    }
+
+    public void setNotify(boolean notify) {
+        isNotify = notify;
+    }
 }
