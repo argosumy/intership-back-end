9974926888e9480464ce85aa2e9663234f329863
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-01 15:17:19 +0300

    BE-P-004 Add PersistenceUserBlockService :: isUserAlreadyBlocked() method.

--- src/main/java/com/spd/baraholka/user/persistance/PersistenceUserBlockService.java
+++ src/main/java/com/spd/baraholka/user/persistance/PersistenceUserBlockService.java
@@ -2,7 +2,11 @@ package com.spd.baraholka.user.persistance;
 
 import com.spd.baraholka.user.persistance.entities.BlockDetail;
 
+import java.util.Optional;
+
 public interface PersistenceUserBlockService {
 
     int insertBlockDetails(BlockDetail blockDetailDTO);
+
+    Optional<Boolean> isUserAlreadyBlocked(int id);
 }
