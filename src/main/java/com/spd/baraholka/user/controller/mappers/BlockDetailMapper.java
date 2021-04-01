22ae0aa8958b93ef40927b556535ae4e21d15a41
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-01 14:37:29 +0300

    BE-P-004 Create BlockDetailMapper with implementation.

---
+++ src/main/java/com/spd/baraholka/user/controller/mappers/BlockDetailMapper.java
@@ -0,0 +1,18 @@
+package com.spd.baraholka.user.controller.mappers;
+
+import com.spd.baraholka.user.controller.dto.BlockDetailDTO;
+import com.spd.baraholka.user.persistance.entities.BlockDetail;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BlockDetailMapper {
+
+    public BlockDetail convertToEntity(BlockDetailDTO blockDetailDTO) {
+        BlockDetail blockDetail = new BlockDetail();
+        blockDetail.setUserId(blockDetailDTO.getUserId());
+        blockDetail.setBanedUntil(blockDetailDTO.getBanedUntil());
+        blockDetail.setReason(blockDetailDTO.getReason());
+        blockDetail.setNotify(blockDetailDTO.isNotify());
+        return blockDetail;
+    }
+}
