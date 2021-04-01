9835f87f761e4348e57277dd523c51832d94aa36
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-01 14:02:32 +0300

    BE-P-004 Create UserBlockService class.

---
+++ src/main/java/com/spd/baraholka/user/service/UserBlockService.java
@@ -0,0 +1,12 @@
+package com.spd.baraholka.user.service;
+
+import com.spd.baraholka.user.controller.dto.BanDetailDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserBlockService {
+
+    public int changeUserBlockedStatus(BanDetailDTO banDetailDTO) {
+        return 0;
+    }
+}
