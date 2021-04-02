8558d52d8abfb1aea47be12206e80dd7b2d2afab
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 12:58:05 +0300

    BE-P-004 Implement BlockDetailRepository :: selectShortBlockDetailInfo() method.

--- src/main/java/com/spd/baraholka/user/persistance/repositories/BlockDetailRepository.java
+++ src/main/java/com/spd/baraholka/user/persistance/repositories/BlockDetailRepository.java
@@ -2,6 +2,8 @@ package com.spd.baraholka.user.persistance.repositories;
 
 import com.spd.baraholka.user.persistance.PersistenceUserBlockService;
 import com.spd.baraholka.user.persistance.entities.BlockDetail;
+import com.spd.baraholka.user.persistance.mappers.BlockDetailsRowMapper;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -16,13 +18,16 @@ import java.util.Optional;
 public class BlockDetailRepository implements PersistenceUserBlockService {
 
     private static final String INSERT_SQL =
-            "INSERT INTO users_block_details (user_id, baned_until, reason, is_notify) VALUES (:userId, :banedUntil, :reason, :isNotify) RETURNING id";
+            "INSERT INTO users_block_details (user_id, blocked_until, reason, is_notify) VALUES (:userId, :blockedUntil, :reason, :isNotify) RETURNING id";
     private static final String IS_EXIST_SQL = "SELECT count(*) <> 0 FROM users_block_details WHERE user_id=:id";
-    private static final String UPDATE_SQL = "UPDATE users_block_details SET baned_until=:banedUntil, reason=:reason, is_notify=:isNotify WHERE user_id=:userId";
+    private static final String UPDATE_SQL = "UPDATE users_block_details SET blocked_until=:blockedUntil, reason=:reason, is_notify=:isNotify WHERE user_id=:userId";
+    private static final String SHORT_VIEW_SELECT_SQL = "SELECT is_blocked, blocked_until FROM users_block_details WHERE user_id=:userId";
     private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final BlockDetailsRowMapper blockDetailsRowMapper;
 
-    public BlockDetailRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+    public BlockDetailRepository(NamedParameterJdbcTemplate jdbcTemplate, BlockDetailsRowMapper blockDetailsRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.blockDetailsRowMapper = blockDetailsRowMapper;
     }
 
     @Override
@@ -45,8 +50,17 @@ public class BlockDetailRepository implements PersistenceUserBlockService {
         return blockDetail.getUserId();
     }
 
+    @Override
+    public Optional<BlockDetail> selectShortBlockDetailInfo(int userId) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(SHORT_VIEW_SELECT_SQL, Map.of("userId", userId), blockDetailsRowMapper));
+        } catch (DataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
     private Map<String, ? extends Comparable<? extends Comparable<?>>> createUpdateParameters(BlockDetail blockDetail) {
-        return Map.of("banedUntil", blockDetail.getBanedUntil(),
+        return Map.of("blockedUntil", blockDetail.getBlockedUntil(),
                 "reason", blockDetail.getReason(),
                 "isNotify", blockDetail.isNotify(),
                 "userId", blockDetail.getUserId());
@@ -55,7 +69,7 @@ public class BlockDetailRepository implements PersistenceUserBlockService {
     private MapSqlParameterSource createInsertParameters(BlockDetail blockDetailDTO) {
         MapSqlParameterSource namedParameters = new MapSqlParameterSource();
         namedParameters.addValue("userId", blockDetailDTO.getUserId());
-        namedParameters.addValue("banedUntil", Timestamp.valueOf(blockDetailDTO.getBanedUntil()));
+        namedParameters.addValue("blockedUntil", Timestamp.valueOf(blockDetailDTO.getBlockedUntil()));
         namedParameters.addValue("reason", blockDetailDTO.getReason());
         namedParameters.addValue("isNotify", blockDetailDTO.isNotify());
         return namedParameters;
