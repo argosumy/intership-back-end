a7a60fd0f9438b159cc7e1d747b18f6f59bcd8cf
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 12:22:28 +0300

    BE-P-004 Create BlockDetailsRowMapper class with implementation.

---
+++ src/main/java/com/spd/baraholka/user/persistance/mappers/BlockDetailsRowMapper.java
@@ -0,0 +1,28 @@
+package com.spd.baraholka.user.persistance.mappers;
+
+import com.spd.baraholka.user.persistance.entities.BlockDetail;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Component
+public class BlockDetailsRowMapper implements RowMapper<BlockDetail> {
+
+    @Override
+    public BlockDetail mapRow(ResultSet rs, int rowNum) throws SQLException {
+        BlockDetail blockDetail = new BlockDetail();
+        blockDetail.setBlocked(rs.getBoolean("is_blocked"));
+        setBannedUntil(rs, blockDetail);
+        return blockDetail;
+    }
+
+    private void setBannedUntil(ResultSet resultSet, BlockDetail blockDetail) throws SQLException {
+        Timestamp blockedUntilTimeStamp = resultSet.getTimestamp("blocked_until");
+        LocalDateTime blockedUntilDate = blockedUntilTimeStamp.toLocalDateTime();
+        blockDetail.setBlockedUntil(blockedUntilDate);
+    }
+}
