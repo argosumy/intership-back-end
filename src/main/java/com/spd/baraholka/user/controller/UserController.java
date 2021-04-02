3018afcd18d6c3708666af5ddb57c99be70caef4
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 11:18:26 +0300

    BE-P-004 Start user UserProfileService in UserController instead of set of different services.

--- src/main/java/com/spd/baraholka/user/controller/UserController.java
+++ src/main/java/com/spd/baraholka/user/controller/UserController.java
@@ -4,14 +4,12 @@ import com.spd.baraholka.annotation.user.UserExist;
 import com.spd.baraholka.user.controller.dto.BlockDetailDTO;
 import com.spd.baraholka.user.controller.dto.UserDTO;
 import com.spd.baraholka.user.controller.dto.UserShortViewDTO;
-import com.spd.baraholka.user.service.UserBlockService;
-import com.spd.baraholka.user.service.UserService;
+import com.spd.baraholka.user.service.UserProfileService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.spd.baraholka.user.service.UserSettingsService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,14 +20,10 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserService userService;
-    private final UserSettingsService userSettingsService;
-    private final UserBlockService userBlockService;
+    private final UserProfileService userService;
 
-    public UserController(UserService userService, UserSettingsService userSettingsService, UserBlockService userBlockService) {
+    public UserController(UserProfileService userService) {
         this.userService = userService;
-        this.userSettingsService = userSettingsService;
-        this.userBlockService = userBlockService;
     }
 
     @GetMapping("/{id}")
@@ -39,7 +33,7 @@ public class UserController {
 
     @PutMapping("/{id}/settings/general")
     public int updateUserGeneralSettings(@PathVariable("id") @UserExist int id, @RequestParam("openAdsInNewTab") boolean openAdsInNewTab) {
-        return userSettingsService.updateUserGeneralSettings(id, openAdsInNewTab);
+        return userService.updateUserGeneralSettings(id, openAdsInNewTab);
     }
 
     @GetMapping
@@ -49,6 +43,6 @@ public class UserController {
 
     @PutMapping("/settings/block")
     public int changeUserBlockedStatus(@RequestBody @Valid BlockDetailDTO blockDetailDTO) {
-        return userBlockService.blockUser(blockDetailDTO);
+        return userService.blockUser(blockDetailDTO);
     }
 }
