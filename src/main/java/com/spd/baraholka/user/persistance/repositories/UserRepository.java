beb55b43449ae5b15fae3d4fe1994ed4f808a911
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 19:59:55 +0300

    BE-P-004 Extract sql statements to class level and refactor UserRepository :: create() method.

--- src/main/java/com/spd/baraholka/user/persistance/repositories/UserRepository.java
+++ src/main/java/com/spd/baraholka/user/persistance/repositories/UserRepository.java
@@ -24,6 +24,15 @@ import java.util.Set;
 @Repository
 public class UserRepository implements PersistenceUserService {
 
+    private static final String SELECT_USER_BY_ID_SQL = "SELECT * FROM users WHERE id=:id";
+    private static final String SELECT_ALL_USERS_SQL = "SELECT id, first_name, last_name, avatar, e_mail FROM users";
+    private static final String EXIST_BY_EMAIL_SQL = "SELECT count(*) <> 0 FROM users WHERE LOWER (e_mail) = LOWER (:email)";
+    private static final String USER_COUNT_SQL = "SELECT count(*) FROM users";
+    private static final String EXIST_BY_ID_SQL = "SELECT count(*) <> 0 FROM users WHERE id=:id";
+    private static final String SELECT_OWNER_SQL = "SELECT id, first_name, last_name, avatar, e_mail FROM users WHERE id=:id";
+    private static final String SAVE_USER_ROLE_SQL = "INSERT INTO users_roles (user_id, role) values (:user_id, :role)";
+    private static final String INSERT_USER_SQL = "INSERT INTO users (first_name, last_name, e_mail, location, phone_number, position, avatar) " +
+            "VALUES (:first_name, :last_name, :email, :location, :phone_number, :position, :avatar) ";
     private final NamedParameterJdbcTemplate jdbcTemplate;
     private final UserRowMapper userRowMapper;
     private final UserShortViewRowMapper userShortViewRowMapper;
@@ -41,38 +50,24 @@ public class UserRepository implements PersistenceUserService {
 
     @Override
     public User selectUserById(int id) {
-        String selectSQL = "SELECT * FROM users WHERE id=:id";
-        Map<String, Integer> selectParameters = Map.of("id", id);
-        return jdbcTemplate.queryForObject(selectSQL, selectParameters, userRowMapper);
+        return jdbcTemplate.queryForObject(SELECT_USER_BY_ID_SQL, Map.of("id", id), userRowMapper);
     }
 
     @Override
     public List<User> selectAllUsers() {
-        String selectSQL = "SELECT id, first_name, last_name, avatar, e_mail FROM users";
-        return jdbcTemplate.query(selectSQL, userShortViewRowMapper);
+        return jdbcTemplate.query(SELECT_ALL_USERS_SQL, userShortViewRowMapper);
     }
 
     @Override
     public Optional<Boolean> existsByEmail(String email) {
-        return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT count(*) <> 0 FROM users WHERE LOWER (e_mail) = LOWER (:email)",
-                Map.of("email", email), Boolean.class));
+        return Optional.ofNullable(jdbcTemplate.queryForObject(EXIST_BY_EMAIL_SQL, Map.of("email", email), Boolean.class));
     }
 
     @Override
     public User create(User user) {
-        final String sql = "INSERT INTO users (first_name, last_name, e_mail, location, phone_number, position, avatar) " +
-                "VALUES (:first_name, :last_name, :email, :location, :phone_number, :position, :avatar) ";
-
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        SqlParameterSource parameters = new MapSqlParameterSource()
-                .addValue("first_name", user.getFirstName())
-                .addValue("last_name", user.getLastName())
-                .addValue("email", user.getEmail())
-                .addValue("location", user.getLocation())
-                .addValue("phone_number", user.getPhoneNumber())
-                .addValue("position", user.getPosition())
-                .addValue("avatar", user.getImageUrl());
-        jdbcTemplate.update(sql, parameters, keyHolder);
+        SqlParameterSource parameters = createInsertUserParameters(user);
+        jdbcTemplate.update(INSERT_USER_SQL, parameters, keyHolder);
         Map<String, Object> keys = Objects.requireNonNull(keyHolder.getKeys());
         if (keys.containsKey("id")) {
             Integer userId = (Integer) keys.get("id");
@@ -84,34 +79,43 @@ public class UserRepository implements PersistenceUserService {
 
     @Override
     public Optional<Integer> count() {
-        return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT count(*) FROM users", Map.of(), Integer.class));
+        return Optional.ofNullable(jdbcTemplate.queryForObject(USER_COUNT_SQL, Map.of(), Integer.class));
     }
 
     @Override
     public Optional<Boolean> isExist(int id) {
-        String isExistQuery = "SELECT count(*) <> 0 FROM users WHERE id=:id";
-        return Optional.ofNullable(jdbcTemplate.queryForObject(isExistQuery, Map.of("id", id), Boolean.class));
+        return Optional.ofNullable(jdbcTemplate.queryForObject(EXIST_BY_ID_SQL, Map.of("id", id), Boolean.class));
     }
 
     @Override
     public Optional<Owner> selectOwner(int id) {
-        String selectSQL = "SELECT id, first_name, last_name, avatar, e_mail FROM users WHERE id=:id";
         try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject(selectSQL, Map.of("id", id), ownerRowMapper));
+            return Optional.ofNullable(jdbcTemplate.queryForObject(SELECT_OWNER_SQL, Map.of("id", id), ownerRowMapper));
         } catch (DataAccessException e) {
             return Optional.empty();
         }
     }
 
+    private MapSqlParameterSource createInsertUserParameters(User user) {
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        namedParameters.addValue("first_name", user.getFirstName());
+        namedParameters.addValue("last_name", user.getLastName());
+        namedParameters.addValue("email", user.getEmail());
+        namedParameters.addValue("location", user.getLocation());
+        namedParameters.addValue("phone_number", user.getPhoneNumber());
+        namedParameters.addValue("position", user.getPosition());
+        namedParameters.addValue("avatar", user.getImageUrl());
+        return namedParameters;
+    }
+
     private void saveUserRoles(User user) {
         Set<Role> roles = Objects.requireNonNull(user.getRoles());
         int userId = user.getId();
-        final String sql = "INSERT INTO users_roles (user_id, role) values (:user_id, :role)";
         for (Role role : roles) {
             SqlParameterSource parameters = new MapSqlParameterSource()
                     .addValue("user_id", userId)
                     .addValue("role", role.name());
-            jdbcTemplate.update(sql, parameters);
+            jdbcTemplate.update(SAVE_USER_ROLE_SQL, parameters);
         }
     }
 }
