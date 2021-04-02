6cb63c89f7d31192d5c6a586466beb3af4a86fa7
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 12:59:02 +0300

    BE-P-004 Add UserBlockService :: getShortViewUserBlockDetail() and createDefaultDTO() methods.

--- src/main/java/com/spd/baraholka/user/service/UserBlockService.java
+++ src/main/java/com/spd/baraholka/user/service/UserBlockService.java
@@ -1,10 +1,13 @@
 package com.spd.baraholka.user.service;
 
 import com.spd.baraholka.user.controller.dto.BlockDetailDTO;
+import com.spd.baraholka.user.controller.dto.ShortViewBlockDetailDTO;
 import com.spd.baraholka.user.controller.mappers.BlockDetailMapper;
 import com.spd.baraholka.user.persistance.PersistenceUserBlockService;
 import com.spd.baraholka.user.persistance.entities.BlockDetail;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserBlockService {
@@ -26,5 +29,21 @@ public class UserBlockService {
         } else {
             return persistenceUserBlockService.insertBlockDetails(blockDetail);
         }
+    }
+
+    public ShortViewBlockDetailDTO getShortViewUserBlockDetail(int userId) {
+        Optional<BlockDetail> blockDetail = persistenceUserBlockService.selectShortBlockDetailInfo(userId);
+        if (blockDetail.isPresent()) {
+            return blockDetailMapper.convertToDTO(blockDetail.get());
+        } else {
+            return createDefaultDTO();
+        }
+    }
+
+    private ShortViewBlockDetailDTO createDefaultDTO() {
+        ShortViewBlockDetailDTO blockDetailDTO = new ShortViewBlockDetailDTO();
+        blockDetailDTO.setBlocked(false);
+        blockDetailDTO.setBlockedUntil(null);
+        return blockDetailDTO;
     }
 }
