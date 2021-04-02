8514662e1e5f3dc532aaa7aa0d0555bf5302ff18
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 11:19:38 +0300

    BE-P-004 Extract logic of work with additional resources into specific service.

--- src/main/java/com/spd/baraholka/user/service/UserService.java
+++ src/main/java/com/spd/baraholka/user/service/UserService.java
@@ -1,15 +1,11 @@
 package com.spd.baraholka.user.service;
 
 import com.spd.baraholka.login.controller.dto.OAuth2UserDTO;
-import com.spd.baraholka.user.controller.dto.UserAdditionalResourceDTO;
-import com.spd.baraholka.user.controller.dto.UserShortViewDTO;
-import com.spd.baraholka.user.controller.mappers.UserAdditionalResourceMapper;
 import com.spd.baraholka.user.controller.dto.UserDTO;
+import com.spd.baraholka.user.controller.dto.UserShortViewDTO;
 import com.spd.baraholka.user.controller.mappers.UserMapper;
-import com.spd.baraholka.user.persistance.PersistenceUserAdditionalResourcesService;
 import com.spd.baraholka.user.persistance.PersistenceUserService;
 import com.spd.baraholka.user.persistance.entities.User;
-import com.spd.baraholka.user.persistance.entities.UserAdditionalResource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,31 +15,16 @@ import java.util.Optional;
 public class UserService {
 
     private final PersistenceUserService persistenceUserService;
-    private final PersistenceUserAdditionalResourcesService persistenceResourceService;
     private final UserMapper userMapper;
-    private final UserAdditionalResourceMapper resourceMapper;
 
-    public UserService(PersistenceUserService persistenceUserService,
-                       PersistenceUserAdditionalResourcesService persistenceResourceService,
-                       UserMapper userMapper,
-                       UserAdditionalResourceMapper resourceMapper) {
+    public UserService(PersistenceUserService persistenceUserService, UserMapper userMapper) {
         this.persistenceUserService = persistenceUserService;
-        this.persistenceResourceService = persistenceResourceService;
         this.userMapper = userMapper;
-        this.resourceMapper = resourceMapper;
     }
 
     public UserDTO getUserById(int id) {
         User user = persistenceUserService.selectUserById(id);
-        return collectUserDTO(user);
-    }
-
-    private UserDTO collectUserDTO(User user) {
-        List<UserAdditionalResource> additionalResources = persistenceResourceService.selectUserAdditionalResources(user.getId());
-        UserDTO userDTO = userMapper.convertToDTO(user);
-        List<UserAdditionalResourceDTO> additionalResourceDTO = resourceMapper.convertToDTOList(additionalResources);
-        userDTO.setAdditionalContactResources(additionalResourceDTO);
-        return userDTO;
+        return userMapper.convertToDTO(user);
     }
 
     public List<UserShortViewDTO> getAllUsers() {
