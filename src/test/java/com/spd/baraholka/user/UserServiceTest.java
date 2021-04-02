ba24e5ed6366d5a0744b6cdd0b323f97f52a80fe
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 11:19:52 +0300

    BE-P-004 Test fix.

--- src/test/java/com/spd/baraholka/user/UserServiceTest.java
+++ src/test/java/com/spd/baraholka/user/UserServiceTest.java
@@ -27,23 +27,14 @@ class UserServiceTest {
     private PersistenceUserService persistenceUserService;
 
     @Mock
-    private PersistenceUserAdditionalResourcesService persistenceUserAdditionalResourcesService;
-
-    @Mock
     private UserMapper userMapper;
-
-    @Mock
-    private UserAdditionalResourceMapper userAdditionalResourceMapper;
 
     @InjectMocks
     private UserService userServiceUnderTest;
 
     @BeforeEach
     void init() {
-        userServiceUnderTest = new UserService(persistenceUserService,
-                persistenceUserAdditionalResourcesService,
-                userMapper,
-                userAdditionalResourceMapper);
+        userServiceUnderTest = new UserService(persistenceUserService, userMapper);
     }
 
     @Test
