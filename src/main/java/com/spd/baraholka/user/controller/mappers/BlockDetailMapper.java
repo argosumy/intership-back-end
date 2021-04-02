16c2f1f0810659dbec0bdfa4676c8ca1f27d324f
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 17:38:12 +0300

    BE-P-004 Rename BlockDetailMapper :: convertToDTO() method into convertToShortViewDTO() and add new convertToDTO() method.

--- src/main/java/com/spd/baraholka/user/controller/mappers/BlockDetailMapper.java
+++ src/main/java/com/spd/baraholka/user/controller/mappers/BlockDetailMapper.java
@@ -1,6 +1,7 @@
 package com.spd.baraholka.user.controller.mappers;
 
-import com.spd.baraholka.user.controller.dto.BlockDetailDTO;
+import com.spd.baraholka.user.controller.dto.EditBlockDetailDTO;
+import com.spd.baraholka.user.controller.dto.FullBlockDetailDTO;
 import com.spd.baraholka.user.controller.dto.ShortViewBlockDetailDTO;
 import com.spd.baraholka.user.persistance.entities.BlockDetail;
 import org.springframework.stereotype.Component;
@@ -11,16 +12,16 @@ import java.util.stream.Collectors;
 @Component
 public class BlockDetailMapper {
 
-    public BlockDetail convertToEntity(BlockDetailDTO blockDetailDTO) {
+    public BlockDetail convertToEntity(EditBlockDetailDTO editBlockDetailDTO) {
         BlockDetail blockDetail = new BlockDetail();
-        blockDetail.setUserId(blockDetailDTO.getUserId());
-        blockDetail.setBlockedUntil(blockDetailDTO.getBanedUntil());
-        blockDetail.setReason(blockDetailDTO.getReason());
-        blockDetail.setNotify(blockDetailDTO.isNotify());
+        blockDetail.setUserId(editBlockDetailDTO.getUserId());
+        blockDetail.setBlockedUntil(editBlockDetailDTO.getBanedUntil());
+        blockDetail.setReason(editBlockDetailDTO.getReason());
+        blockDetail.setNotify(editBlockDetailDTO.isNotify());
         return blockDetail;
     }
 
-    public ShortViewBlockDetailDTO convertToDTO(BlockDetail blockDetail) {
+    public ShortViewBlockDetailDTO convertToShortViewDTO(BlockDetail blockDetail) {
         ShortViewBlockDetailDTO blockDetailDTO = new ShortViewBlockDetailDTO();
         blockDetailDTO.setUserId(blockDetail.getUserId());
         blockDetailDTO.setBlocked(blockDetail.isBlocked());
@@ -28,7 +29,17 @@ public class BlockDetailMapper {
         return blockDetailDTO;
     }
 
+    public FullBlockDetailDTO convertToDTO(BlockDetail blockDetail) {
+        FullBlockDetailDTO blockDetailDTO = new FullBlockDetailDTO();
+        blockDetailDTO.setUserId(blockDetail.getUserId());
+        blockDetailDTO.setReason(blockDetail.getReason());
+        blockDetailDTO.setBlockedUntil(blockDetail.getBlockedUntil());
+        blockDetailDTO.setNotify(blockDetail.isNotify());
+        blockDetailDTO.setBlocked(blockDetail.isBlocked());
+        return blockDetailDTO;
+    }
+
     public List<ShortViewBlockDetailDTO> convertToDTOList(List<BlockDetail> blockDetails) {
-        return blockDetails.stream().map(this::convertToDTO).collect(Collectors.toList());
+        return blockDetails.stream().map(this::convertToShortViewDTO).collect(Collectors.toList());
     }
 }
