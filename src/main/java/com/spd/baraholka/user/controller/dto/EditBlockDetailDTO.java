9137c3ed9fea9987887857361b140934b83211ce
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 17:38:36 +0300

    BE-P-004 Rename BlockDetailDTO into EditBlockDetailDTO.

--- src/main/java/com/spd/baraholka/user/controller/dto/EditBlockDetailDTO.java
+++ src/main/java/com/spd/baraholka/user/controller/dto/EditBlockDetailDTO.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-public class BlockDetailDTO {
+public class EditBlockDetailDTO {
 
     @NotNull
     @Min(1)
