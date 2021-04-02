ab1e9d96bc92a50c183df311ac2f78a95993f64b
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-02 12:17:08 +0300

    BE-P-004 Rename 'banned_until' column into 'blocked_until'.

--- src/main/resources/db/migration/V20210401135338__create_users_block_details_table.sql
+++ src/main/resources/db/migration/V20210401135338__create_users_block_details_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users_block_details
 (
-    id          SERIAL PRIMARY KEY NOT NULL,
-    user_id     int                NOT NULL,
-    baned_until TIMESTAMP,
-    reason      VARCHAR(200),
-    is_notify   BOOLEAN DEFAULT false,
-    is_blocked  BOOLEAN DEFAULT true
+    id            SERIAL PRIMARY KEY NOT NULL,
+    user_id       int                NOT NULL,
+    blocked_until TIMESTAMP,
+    reason        VARCHAR(200),
+    is_notify     BOOLEAN DEFAULT false,
+    is_blocked    BOOLEAN DEFAULT true
 
 );
