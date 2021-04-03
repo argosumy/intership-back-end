cb9b89c4621a15483649577cc3a88f99614eb733
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-03 11:18:48 +0300

    BE-P-003 Add get/set methods for EditMainInfoUserDTO class.

--- src/main/java/com/spd/baraholka/user/controller/dto/EditMainInfoUserDTO.java
+++ src/main/java/com/spd/baraholka/user/controller/dto/EditMainInfoUserDTO.java
@@ -9,4 +9,44 @@ public class EditMainInfoUserDTO {
     private String phoneNumber;
     private String location;
     private List<UserAdditionalResourceDTO> additionalContactResources;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<UserAdditionalResourceDTO> getAdditionalContactResources() {
+        return additionalContactResources;
+    }
+
+    public void setAdditionalContactResources(List<UserAdditionalResourceDTO> additionalContactResources) {
+        this.additionalContactResources = additionalContactResources;
+    }
 }
