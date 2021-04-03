3b9d5a98703a6b7835612a877aa0ffee8e82328c
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-03 11:19:28 +0300

    BE-P-003 Start work with EditMainInfoUserDTO class instead of UserDTO in UserService :: updateUserMainInfo() method.

--- src/main/java/com/spd/baraholka/user/service/UserService.java
+++ src/main/java/com/spd/baraholka/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.spd.baraholka.user.service;
 
 import com.spd.baraholka.login.controller.dto.OAuth2UserDTO;
+import com.spd.baraholka.user.controller.dto.EditMainInfoUserDTO;
 import com.spd.baraholka.user.controller.dto.UserAdditionalResourceDTO;
 import com.spd.baraholka.user.controller.dto.UserShortViewDTO;
 import com.spd.baraholka.user.controller.mappers.UserAdditionalResourceMapper;
@@ -76,10 +77,10 @@ public class UserService {
     }
 
     @Transactional
-    public int updateUserMainInfo(UserDTO userDTO) {
-        User user = userMapper.convertToEntity(userDTO);
+    public int updateUserMainInfo(EditMainInfoUserDTO mainInfoDTO) {
+        User user = userMapper.convertToEntity(mainInfoDTO);
         List<UserAdditionalResource> additionalResources =
-                resourceMapper.convertToEntityList(userDTO.getAdditionalContactResources());
+                resourceMapper.convertToEntityList(mainInfoDTO.getAdditionalContactResources());
         persistenceResourceService.updateUserAdditionalResources(additionalResources);
         return persistenceUserService.updateUserMainInfo(user);
     }
