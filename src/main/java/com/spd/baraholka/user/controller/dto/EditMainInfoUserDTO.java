d14bd2eaa3db22a797bca3b57eff866ae2769f73
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-03 11:14:38 +0300

    BE-P-003 Create EditMainInfoUserDTO class.

---
+++ src/main/java/com/spd/baraholka/user/controller/dto/EditMainInfoUserDTO.java
@@ -0,0 +1,12 @@
+package com.spd.baraholka.user.controller.dto;
+
+import java.util.List;
+
+public class EditMainInfoUserDTO {
+
+    private int userId;
+    private String position;
+    private String phoneNumber;
+    private String location;
+    private List<UserAdditionalResourceDTO> additionalContactResources;
+}
