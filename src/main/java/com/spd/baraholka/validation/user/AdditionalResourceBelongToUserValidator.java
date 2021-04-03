452e160685f9bb175c02002250671ae76686d200
Oleksandr Sytnianskyi <sitnyanskij.aleksandr@gmail.com>
2021-04-03 13:40:13 +0300

    BE-P-003 implement validation in AdditionalResourceBelongToUserValidator class.

--- src/main/java/com/spd/baraholka/validation/user/AdditionalResourceBelongToUserValidator.java
+++ src/main/java/com/spd/baraholka/validation/user/AdditionalResourceBelongToUserValidator.java
@@ -2,14 +2,29 @@ package com.spd.baraholka.validation.user;
 
 import com.spd.baraholka.annotation.user.BelongToUser;
 import com.spd.baraholka.user.controller.dto.UserAdditionalResourceDTO;
+import com.spd.baraholka.user.service.UserService;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.List;
 
 public class AdditionalResourceBelongToUserValidator implements ConstraintValidator<BelongToUser, UserAdditionalResourceDTO> {
 
+    private final UserService userService;
+
+    public AdditionalResourceBelongToUserValidator(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public boolean isValid(UserAdditionalResourceDTO value, ConstraintValidatorContext context) {
-        return true; //TODO add validation by belonging to user
+        int id = value.getId();
+        int userId = value.getUserId();
+        if (id != 0) {
+            List<Integer> additionalResourcesId = userService.getUserAdditionalResourcesId(userId);
+            return additionalResourcesId.stream().anyMatch(integer -> integer == id);
+        } else {
+            return true;
+        }
     }
 }
