e9cc36f1d4739e3ae07d4b51f33f27973c115d16
Valeriia Lapytska <lerkasan@gmail.com>
2021-04-05 19:44:17 -0400

    [BE-P-015] fixes for ci pipeline

--- src/test/java/com/spd/baraholka/comments/controllers/CommentControllerTest.java
+++ src/test/java/com/spd/baraholka/comments/controllers/CommentControllerTest.java
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -37,7 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc(addFilters = false)
-@WebMvcTest(CommentController.class)
+@SpringBootTest
 class CommentControllerTest {
 
     @Autowired
