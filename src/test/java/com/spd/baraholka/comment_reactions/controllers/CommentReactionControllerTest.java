f1cfe22f98efe2fb4dacb0027298a39e12d73841
Valeriia Lapytska <lerkasan@gmail.com>
2021-04-06 02:23:39 -0400

    Revert "[be-012] added populateDatabase task for Gradle"
    [BE-P-015] added @ActiveProfiles("test") for two test classes

--- src/test/java/com/spd/baraholka/comment_reactions/controllers/CommentReactionControllerTest.java
+++ src/test/java/com/spd/baraholka/comment_reactions/controllers/CommentReactionControllerTest.java
@@ -22,6 +22,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -33,6 +34,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest
 class CommentReactionControllerTest {
--- src/test/java/com/spd/baraholka/comments/controllers/CommentControllerTest.java
+++ src/test/java/com/spd/baraholka/comments/controllers/CommentControllerTest.java
@@ -24,6 +24,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -36,6 +37,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest
 class CommentControllerTest {
